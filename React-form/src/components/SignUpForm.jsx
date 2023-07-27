8e67dcc4c1912a592550dafa0e2ebbe47b84a9d3
Abdoul Akano <akano1nassirou@gmail.com>
2023-07-27 10:07:30 -0400

    sign up form done

--- React-form/src/components/SignUpForm.jsx
+++ React-form/src/components/SignUpForm.jsx
@@ -1,30 +1,42 @@
 import {useState} from "react"
-const [username, setUsername]= useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState(null);
 
 export default function SignUpForm() {
-    
+    const [username, setUsername]= useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
     async function handleSubmit(event) {
+         event.preventDefault();
         try {
-            event.preventDefault();
-            console.log("Hello, ..)")
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
+            const result = await response.json();
+            console.log(result);
         } catch (error) {
-            console.log(error);
+            setError(error.message);
         }
 
     }
     return (
-        <h2>Sign Up!</h2>;
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username : <input  value={username} onChange={(e)=> setUsername(e.target.value)} />
-            </label>
-            <label>
-                password : <input value = {password} onChange={(e)=> setPassword(e.target.value)}/>
-            </label>
-            <button>submit</button>
-        </form>
+        <>
+            <h2>Sign Up!</h2>
 
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username : <input 
+                    type="text" 
+                    value={username} 
+                    onChange={(e)=> setUsername(e.target.value)} />
+                </label>
+                <label>
+                    password : <input 
+                    type="password" 
+                    value = {password} 
+                    onChange={(e)=> setPassword(e.target.value)}/>
+                </label>
+                <button>submit</button>
+            </form>
+
+        </>
+       
     )
   }
